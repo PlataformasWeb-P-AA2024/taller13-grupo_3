acac9ebbb9054d544cc76b0cd63182b0ff244e27
uuunin <rdcarrion3@utpl.edu.ec>
2024-07-15 19:59:30 -0500

    Cambios de urls

--- taller/inmobiliaria/propiedades/views.py
+++ taller/inmobiliaria/propiedades/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import viewsets
+from .models import Edificio, Departamento
+from .serializers import EdificioSerializer, DepartamentoSerializer
+
+class EdificioViewSet(viewsets.ModelViewSet):
+    queryset = Edificio.objects.all()
+    serializer_class = EdificioSerializer
+
+class DepartamentoViewSet(viewsets.ModelViewSet):
+    queryset = Departamento.objects.all()
+    serializer_class = DepartamentoSerializer
